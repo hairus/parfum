c69fde86bb5e4e268b48f16b2a8047244c0b8b10
hairus sabilah <zhomenep@gmail.com>
2023-11-11 03:49:11 +0700

    add loading

--- resources/views/sc.blade.php
+++ resources/views/sc.blade.php
@@ -10,6 +10,7 @@
                 <div id="tampung"></div>
                 <div id="modal"></div>
                 <div id="transaksi"></div>
+                <div class="loading" id="loadingDiv"></div>
             </div>
         </div>
     </div>
@@ -25,30 +26,44 @@
             getData()
         })
 
+        function showloading() {
+            var $loading = $('#loadingDiv').show();
+            $loading.show();
+        }
+
+        function hideloading() {
+            var $loading = $('#loadingDiv').hide();
+            $loading.hide();
+        }
+
         function getData() {
+            showloading()
             $.ajax({
                 url: "/getData",
                 type: "GET",
                 success: function(data) {
                     $('#tampung').html(data)
                     $("#example").dataTable()
+                    hideloading()
                 }
             })
         }
 
         function coba(id) {
-            console.log(id);
             $.ajax({
                 url: "/edit/" + id,
                 type: "GET",
                 success: function(data) {
+                    showloading()
                     $('#modal').html(data)
                     $('#exampleModal').modal('show');
+                    hideloading()
                 }
             });
         }
 
         function simpan() {
+            showloading()
             var nama = $('#nama').val()
             var desk = $('#desk').val()
             var id = $('#id').val()
@@ -64,16 +79,19 @@
                 success: function(data) {
                     $('#exampleModal').modal('hide');
                     getData()
+                    hideloading()
                 }
             })
         }
 
         function hapus(id) {
+
             $.confirm({
                 title: 'Confirm!',
                 content: 'Simple confirm!',
                 buttons: {
                     confirm: function() {
+                        showloading()
                         $.ajax({
                             url: "/destroy/" + id,
                             type: "DELETE",
@@ -82,6 +100,7 @@
                             },
                             success: function(data) {
                                 getData()
+                                hideloading()
                             }
                         })
                     },
@@ -101,17 +120,21 @@
         }
         // transaksi
         function show(id) {
+            showloading()
             $.ajax({
                 url: "/show/" + id,
                 type: "get",
                 success: function(data) {
+
                     $('#transaksi').html(data);
                     $('#showModal').modal('show');
+                    hideloading()
                 }
             })
         }
 
         function simpanTrx() {
+            showloading()
             const id = $('#id').val();
             const jum = $('#trx').val();
             $.ajax({
@@ -125,16 +148,19 @@
                 success: function(data) {
                     $('#showModal').modal('hide');
                     getData()
+                    hideloading()
                 }
             })
         }
 
         function claim(id) {
+            showloading()
             $.ajax({
                 url: "/claim/" + id,
                 type: "GET",
                 success: function(data) {
                     getData()
+                    hideloading()
                     $.confirm({
                         title: 'Claim',
                         content: 'Claim berhasil di lakukan',
@@ -144,18 +170,21 @@
             })
         }
 
-        function hapusStempel(id){
+        function hapusStempel(id) {
+            showloading()
             $.ajax({
                 url: "/hapusStempel/" + id,
                 type: "GET",
                 success: function(data) {
                     $('#transaksi').html(data);
                     $('#hapModal').modal('show');
+                    hideloading()
                 }
             })
         }
 
-        function runningHapus(){
+        function runningHapus() {
+
             const id = $('#id').val();
             const jum = $('#trx').val();
             $.ajax({
@@ -167,11 +196,135 @@
                     id: id
                 },
                 success: function(data) {
-                    console.log(data);
+                    showloading()
                     $('#hapModal').modal('hide');
                     getData()
+                    hideloading()
                 }
             })
         }
     </script>
 @endsection
+<style>
+    /* Absolute Center Spinner */
+.loading {
+  position: fixed;
+  z-index: 999;
+  height: 2em;
+  width: 2em;
+  overflow: show;
+  margin: auto;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+
+/* Transparent Overlay */
+.loading:before {
+  content: '';
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+    background: radial-gradient(rgba(20, 20, 20,.8), rgba(0, 0, 0, .8));
+
+  background: -webkit-radial-gradient(rgba(20, 20, 20,.8), rgba(0, 0, 0,.8));
+}
+
+/* :not(:required) hides these rules from IE9 and below */
+.loading:not(:required) {
+  /* hide "loading..." text */
+  font: 0/0 a;
+  color: transparent;
+  text-shadow: none;
+  background-color: transparent;
+  border: 0;
+}
+
+.loading:not(:required):after {
+  content: '';
+  display: block;
+  font-size: 10px;
+  width: 1em;
+  height: 1em;
+  margin-top: -0.5em;
+  -webkit-animation: spinner 150ms infinite linear;
+  -moz-animation: spinner 150ms infinite linear;
+  -ms-animation: spinner 150ms infinite linear;
+  -o-animation: spinner 150ms infinite linear;
+  animation: spinner 150ms infinite linear;
+  border-radius: 0.5em;
+  -webkit-box-shadow: rgba(255,255,255, 0.75) 1.5em 0 0 0, rgba(255,255,255, 0.75) 1.1em 1.1em 0 0, rgba(255,255,255, 0.75) 0 1.5em 0 0, rgba(255,255,255, 0.75) -1.1em 1.1em 0 0, rgba(255,255,255, 0.75) -1.5em 0 0 0, rgba(255,255,255, 0.75) -1.1em -1.1em 0 0, rgba(255,255,255, 0.75) 0 -1.5em 0 0, rgba(255,255,255, 0.75) 1.1em -1.1em 0 0;
+box-shadow: rgba(255,255,255, 0.75) 1.5em 0 0 0, rgba(255,255,255, 0.75) 1.1em 1.1em 0 0, rgba(255,255,255, 0.75) 0 1.5em 0 0, rgba(255,255,255, 0.75) -1.1em 1.1em 0 0, rgba(255,255,255, 0.75) -1.5em 0 0 0, rgba(255,255,255, 0.75) -1.1em -1.1em 0 0, rgba(255,255,255, 0.75) 0 -1.5em 0 0, rgba(255,255,255, 0.75) 1.1em -1.1em 0 0;
+}
+
+/* Animation */
+
+@-webkit-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-moz-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-o-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+</style>
