17bb5cc11d03a54df542d326b1826a82e83e6f47
hairus sabilah <zhomenep@gmail.com>
2023-11-10 22:45:24 +0700

    penghapusan stempel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    THE PARFUM
+                    ANEKA WANGI
                 </div>
             </div>
         </div>
